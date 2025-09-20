c82435f3c07dc286d146a2f7d3b4792b93e18d1c
Riya <taakriya64@gmail.com>
2025-09-19 21:19:26 -0700

    3_triangle

--- Diamond_triangle.js
+++ Diamond_triangle.js
@@ -1,12 +1,69 @@
-     /* 
-         X
-       X X X
-     X X X X X
-   X X X X X X X
- X X X X X X X X X
-   X X X X X X X
-     X X X X X
-       X X X
-         X
+/* 
+        X
+      X X X
+    X X X X X
+  X X X X X X X
+X X X X X X X X X
+  X X X X X X X
+    X X X X X
+      X X X
+        X
 
- */
+*/
+function Diamond_Triangle(n) {
+  // for (let i = 1; i <= n; i++) {
+  //   row = " ";
+  //   for (let j = 1; j <= n; j++) {
+  //     if (j <= n - i) {    if()
+  //       row = row + "  ";
+  //     }
+  //     else {
+  //       row = row + "X ";
+  //     }
+  //   }
+
+  //   for (let j = 1; j < i; j++) {
+  //     row = row + "X ";
+  //   }
+    
+
+  //   console.log(row);
+  // }
+
+
+/*
+  X X X X X X X 
+    X X X X X
+      X X X
+        X
+i=1
+j=1,
+
+
+*/
+
+
+
+
+  for (let i = 1; i <= n; i++) {
+    row = " ";
+    for (let j = 1; j <= n; j++) {
+      if (j <= n - i) {
+        row = row + "X ";
+      }
+      else {
+        row = row + "  ";
+      }
+    }
+
+    for (let j = 1; j < i; j++) {
+      row = row + "X ";
+    }
+    
+
+    console.log(row);
+  }
+}
+
+  let size = 5;
+  Diamond_Triangle(size);
--- Pattern/left_up_triangle.js
+++ Pattern/left_up_triangle.js
@@ -1,11 +1,34 @@
- /*
+/*
 
  
 X
 X X
 X X X
+ 
+X
+X X
+X X X
+
+X
+X X
+X X X
+
+=============
+
+X
+X X
+X X X
 X X X X
-X X X X X
+ 
+X
+X X
+X X X
+X X X X
+
+X
+X X
+X X X
+X X X X
 
 i=1,   2,    3,          4,
 j=1,   1,2,  1, 2 ,3     1,2,3,4
@@ -15,20 +38,30 @@ j=1,2,3,4,5    1,2,3,4
 s=0,           1                  
 */
 
- function printLeftUpTriangle(n)
- {
-    for(let i=1; i<=n; i++)
-    {
-        let row = " ";
-        for(let j=1; j<=i; j++)
-        {
-         row = row + "X ";
-         
+function printLeftUpTriangle(n) {
+    for (let x = 1; x < 3; x++) { // print group of triangle
+
+        for (let a = 1; a <= 3; a++) { // print triangle
+
+            for (let i = 1; i <= (n+x)-1; i++) { // print line
+                let row = " ";
+                for (let j = 1; j <= i; j++) { // print star
+                    row = row + "X ";
+
+                }
+                console.log(row);
+            }
+            console.log(" ");
         }
-            console.log(row);
+        if(x==1)
+        {
+            console.log("=============");
+        }
+
     }
 
- }
+}
 
-let size=5;
+
+let size = 3;
 printLeftUpTriangle(size);
